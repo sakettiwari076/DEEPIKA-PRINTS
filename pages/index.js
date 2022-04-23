641e772e7eba38f35276246e36a4746f282ce312
Saket <sakettiwari076@gmail.com>
2022-04-24 02:36:29 +0530

    Navbar added and contactus

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Navbar from '../Subcomponents/Navbar'
 export default function Home() {
   return (
-    <h1 className="text-8xl font-bold underline ">
-    Hello world!
-  </h1>  )
+
+    <div className=''>
+
+    
+  </div>
+  )
 }
