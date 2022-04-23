a7903bd0fa90783326a61e7f05c0ed1a7d96e41e
Saket <sakettiwari076@gmail.com>
2022-04-24 05:01:44 +0530

    product

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../Subcomponents/Navbar'
 
+
 export default function Home() {
   return (
 
     <div className=' bgour '>
   
     <div className="text-gray-600 body-font">
-    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="container mx-auto flex  py-24 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 fontour pt-4 text-gray-900">Before they sold out,
           
@@ -20,7 +21,7 @@ export default function Home() {
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-        <img className="object-cover object-center rounded" alt="hero" src="giveaw.jpg"/>
+        <img className="object-cover object-center rounded-3xl" alt="hero" src="giveaw.jpg"/>
       </div>
     </div>
   </div>  
