c7ca22ff5fe944772f3a4ac4eb947533c50b3a08
Saket <sakettiwari076@gmail.com>
2022-04-24 17:02:19 +0530

    frontpage

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../Subcomponents/Navbar'
-
-
 export default function Home() {
   return (
-
-    <div className=' bgour '>
-  
+    <div className=' '>
     <div className="text-gray-600 body-font">
     <div className="container mx-auto flex  py-24 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -21,12 +17,43 @@ export default function Home() {
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-        <img className="object-cover object-center rounded-3xl" alt="hero" src="giveaw.jpg"/>
+        <img className="object-cover object-center rounded-3xl" alt="hero" src="https://img.freepik.com/free-vector/document-signing-partnership-deal-business-consultation-work-arrangement-client-assistant-writing-contract-cartoon-characters-vector-isolated-concept-metaphor-illustration_335657-2761.jpg?w=740"/>
       </div>
     </div>
-  </div>  
-<div className='py-24'></div>
-  </div>
+    </div>  
+    <div className=' bgour text-gray-900 py-28 mt-8 grid lg:grid-cols-4 text-center '>
+    <div className='font-semibold text-4xl fontour3'>
+        100+ <br></br> CLIENTS
+        </div>
+        <div className='font-semibold text-4xl fontour3'>
+        100+ <br></br> CLIENTS
+        </div>
+        <div className='font-semibold text-4xl fontour3'>
+        100+ <br></br> CLIENTS
+        </div>
+        <div className='font-semibold text-4xl fontour3'>
+        100+ <br></br> CLIENTS
+        </div>
+   
+   
+        </div>
+     <div className=' bgour text-gray-900 py-28 mt-8 grid lg:grid-cols-4 text-center '>
+ <div className='font-semibold text-4xl fontour3'>
+     100+ <br></br> CLIENTS
+     </div>
+     <div className='font-semibold text-4xl fontour3'>
+     100+ <br></br> CLIENTS
+     </div>
+     <div className='font-semibold text-4xl fontour3'>
+     100+ <br></br> CLIENTS
+     </div>
+     <div className='font-semibold text-4xl fontour3'>
+     100+ <br></br> CLIENTS
+     </div>
+
+
+     </div>
+     </div>
   
   )
 }
