60736ceae6225f6ef45f2802603f476172948267
Saket <sakettiwari076@gmail.com>
2022-04-30 03:04:41 +0530

    navbar removed from non functioning pagesd

--- pages/Components/product.js
+++ pages/Components/product.js
@@ -3,10 +3,12 @@ import React from 'react'
 const product = () => {
   return (
     <div>
+
     <h1 className='text-center py-5 text-5xl font-semibold'>PRODUCTS</h1>
 <div className='mt-8 grid lg:grid-cols-3 py-4 space-x-5 '>
 
 <div className='py-3'>
+
 
 <img src='https://img.freepik.com/free-psd/cosmetic-glass-bottle-packaging-mockup_439185-7968.jpg?t=st=1650790899~exp=1650791499~hmac=b9443be22f9dd048e0e1e80849e83981ebf4a36368be0a1d1ee8c2d6bafdb039&w=900' className=''/>
 <div className=''>TOP</div>
