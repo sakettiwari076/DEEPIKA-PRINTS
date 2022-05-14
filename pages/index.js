3b44a8503d274fdce07be84f07da4b6a13fbd33c
Saket <sakettiwari076@gmail.com>
2022-05-14 20:18:22 +0530

    changed

--- pages/index.js
+++ pages/index.js
@@ -50,84 +50,7 @@ function classNames(...classes) {
 									>
 										Home
 									</Link>
-									<Menu as="div" className="relative inline-block text-left">
-									<div>
-									  <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-										Options
-										<ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-									  </Menu.Button>
-									</div>
-							  
-									<Transition
-									  as={Fragment}
-									  enter="transition ease-out duration-100"
-									  enterFrom="transform opacity-0 scale-95"
-									  enterTo="transform opacity-100 scale-100"
-									  leave="transition ease-in duration-75"
-									  leaveFrom="transform opacity-100 scale-100"
-									  leaveTo="transform opacity-0 scale-95"
-									>
-									  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-										<div className="py-1">
-										  <Menu.Item>
-											{({ active }) => (
-											  <a
-												href="#"
-												className={classNames(
-												  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-												  'block px-4 py-2 text-sm'
-												)}
-											  >
-												Account settings
-											  </a>
-											)}
-										  </Menu.Item>
-										  <Menu.Item>
-											{({ active }) => (
-											  <a
-												href="#"
-												className={classNames(
-												  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-												  'block px-4 py-2 text-sm'
-												)}
-											  >
-												Support
-											  </a>
-											)}
-										  </Menu.Item>
-										  <Menu.Item>
-											{({ active }) => (
-											  <a
-												href="#"
-												className={classNames(
-												  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-												  'block px-4 py-2 text-sm'
-												)}
-											  >
-												License
-											  </a>
-											)}
-										  </Menu.Item>
-										  <form method="POST" action="#">
-											<Menu.Item>
-											  {({ active }) => (
-												<button
-												  type="submit"
-												  className={classNames(
-													active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-													'block w-full text-left px-4 py-2 text-sm'
-												  )}
-												>
-												  Sign out
-												</button>
-											  )}
-											</Menu.Item>
-										  </form>
-										</div>
-									  </Menu.Items>
-									</Transition>
-								  </Menu>
-								)
+								
 							  
                   <Link
                   activeClass=""
@@ -150,10 +73,10 @@ function classNames(...classes) {
 										Testimonails
 									</Link>
 									<Links  href='/diariess'><a className='cursor-pointer hover:bg-blue-600  hover:text-white px-3 py-2 rounded-md text-sm font-medium'> Diaries</a></Links>
-									<Links  href='/bottles'><a className='cursor-pointer hover:bg-blue-600  hover:text-white block text-center py-2 rounded-md text-base font-medium'> Bottles</a></Links>
+									<Links  href='/bottles'><a className='cursor-pointer hover:bg-blue-600  hover:text-white  text-center py-2 rounded-md text-base font-medium'> Bottles</a></Links>
 									
-									<Links  href='/Cup'><a className='cursor-pointer hover:bg-blue-600  hover:text-white block text-center py-2 rounded-md text-base font-medium'> Cup</a></Links>
-									<Links  href='/Fillform'><a className='cursor-pointer hover:bg-blue-600  hover:text-white block text-center py-2 rounded-md text-base font-medium'> One click</a></Links>
+									<Links  href='/cup'><a className='cursor-pointer hover:bg-blue-600  hover:text-white  text-center py-2 rounded-md text-base font-medium'> Mugs</a></Links>
+									<Links  href='/Fillform'><a className='cursor-pointer hover:bg-blue-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium'> One click</a></Links>
 
 
 									<Link
@@ -311,7 +234,7 @@ function classNames(...classes) {
 											  'block px-4 py-2 text-sm'
 											)}
 										  >
-											Cup
+											Mugs
 										  </a>
 										)}
 									  </Menu.Item>
@@ -339,7 +262,7 @@ function classNames(...classes) {
 												'block w-full text-left px-4 py-2 text-sm'
 											  )}
 											>
-											  Comming soon...
+											Pens
 											</button>
 										  )}
 										</Menu.Item>
