71b7e43a6fb92eb188629656b57d7158af6ca6ac
Saket <sakettiwari076@gmail.com>
2022-05-18 23:13:54 +0530

    ss

--- pages/pen.js
+++ pages/pen.js
@@ -1,96 +1,9 @@
 import React from 'react'
-import Link from 'react-scroll/modules/components/Link'
-import Image from 'next'
-
-
-
 
 const pen = () => {
- 
   return (
-    <div>
-    
-    <h1 className='text-center py-5 text-3xl txtnavy font-bold'>Pens</h1>
-       <a href='../'>
-    <img src='https://www.kindpng.com/picc/m/78-780481_inside-page-left-back-button-back-button-png.png' className=' arrow '/>
-    </a>
-<div className='mt-8 grid lg:grid-cols-3 py-4 px-4'>
-
-<div className='py-3'>
-
-<img src= "penn5.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn6.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn7.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn8.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn9.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn10.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn11.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn12.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn13.jpeg" />
-
-
-</div>
-<div className='py-3'>
-
-<img src= "penn14.jpeg" />
-
-
-</div>
-
-
-
-
-
-<div className='py-3 flex justify-center mt-28'>
-
-<a href='' className='text-blue-400 underline font-bold text-2xl' >MORE</a>
-
-</div>
-
-</div>
-</div>
-
+    <div>pen</div>
   )
 }
 
-export default pen;
+export default pen
