9560b6b2a217f8732c4768b3e7a328d47e4989b7
Saket <sakettiwari076@gmail.com>
2022-06-01 13:15:26 +0530

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -32,73 +32,33 @@ const Contact = () => {
   };
 
   return (
-    <StyledContactForm>
-    <div className=" grid lg:grid-cols-2">
-    <div className="">
-    HELLO
-    
-    </div>
+    <div>
+    <h1 className="text-center py-4 font-semibold  lg:text-4xl ">Drop your requirements !</h1>
+<div className="grid lg:grid-cols-2    ">
+<div className="">
+<img className="" src="require.jpg"/>
 
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+</div>
+<div className="py-8 space-y-48 flex justify-center">
+
+      <form ref={form} className="flex flex-col  "  onSubmit={sendEmail}>
+      
+        <label className="font-semibold py-2 text-gray-700 " >Name</label>
+        <input type="text" className="w-96 py-2 bg-gray-200 border-red-500 rounded-md  px-4 mb-3 leading-tight focus:outline-none " name="user_name" />
+        <label className="font-semibold py-2 text-gray-700"> Email</label>
+        <input type="email" className="w-96 py-2 bg-gray-200 " name="user_email" />
+        <label className="font-semibold py-2 text-gray-700"> Message</label>
+        <textarea className="w-96 py-2 bg-gray-200" name="message" />
+        <div className="py-4">
+    <input type="submit" className="bg-blue-900 text-white w-96 py-2"  value="Send" />
+    </div>
       </form>
       </div>
-      </StyledContactForm>
-
+      </div>
+      
+      </div>
   );
 };
 
 export default Contact;
 
-const StyledContactForm = styled.div`
-
-  width: 400px;
-  form {
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    width: 100%;
-    font-size: 16px;
-    input {
-      width: 100%;
-      height: 35px;
-      padding: 7px;
-      outline: none;
-      border-radius: 5px;
-      border: 1px solid rgb(220, 220, 220);
-      &:focus {
-        border: 2px solid rgba(0, 206, 158, 1);
-      }
-    }
-    textarea {
-      max-width: 100%;
-      min-width: 100%;
-      width: 100%;
-      max-height: 100px;
-      min-height: 100px;
-      padding: 7px;
-      outline: none;
-      border-radius: 5px;
-      border: 1px solid rgb(220, 220, 220);
-      &:focus {
-        border: 2px solid rgba(0, 206, 158, 1);
-      }
-    }
-    label {
-      margin-top: 1rem;
-    }
-    input[type="submit"] {
-      margin-top: 2rem;
-      cursor: pointer;
-      background: rgb(249, 105, 14);
-      color: white;
-      border: none;
-    }
-  }
-`;
